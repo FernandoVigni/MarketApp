0f1de4f55c57befebd44b3245a088e6d1c0b9c35
fernandovigni@gmail.com <fernandovigni@gmail.com>
2022-05-31 13:07:13 -0300

    first commit

---
+++ src/test/java/com/MarketApp/MarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MarketApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
